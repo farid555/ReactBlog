794adc2c673690256c2b5e7ed635aa6543fd644e
farid555 <mohammad.uzzaman2@centria.fi>
2021-08-25 01:24:32 +0300

    added auth & users

--- api/index.js
+++ api/index.js
@@ -6,8 +6,10 @@ const mongoose = require("mongoose");
 
 
 const authRoute = require("./routes/auth")
+const userRoute = require("./routes/users")
 
 dotenv.config();
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
@@ -17,6 +19,8 @@ mongoose.connect(process.env.MONGO_URL, {
     .catch((err) => console.log(err));
 
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+
 
 app.listen("5000", () => {
     console.log("Backend is running....")
